b425169697b2c43ae74588410b3eb0a7091cb0e1
TT <codertina@gmail.com>
2018-08-05 17:56:39 +0530

    Tina | Close icon and done icon

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -21,15 +21,22 @@ export default class Main extends React.Component<IMainProps, IMainState> {
                 {task: 'Dance recital'},
             ]
         };
+        this.deleteTodo = this.deleteTodo.bind(this);
+    }
+    deleteTodo(index: number) {
+        const newTodos =
+            [...this.state.todos.slice(0, index), ...this.state.todos.slice(index + 1)];
+        this.setState({todos: newTodos});
     }
 
     render() {
         return <div style={{width: '250px', marginLeft: '500px', marginTop: '51px'}}>
             {this.state.todos
-                .map((todo, index) =>
+                .map((todo: ITodo, index: number) =>
                     <ToDo
                         key={index}
                         todo={{task: todo.task}}
+                        onDelete={() => this.deleteTodo(index)}
                     />)
             }
         </div>
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
-
+import {Icon, Checkbox, Button} from 'semantic-ui-react';
 export interface ITodo {
     task: string;
 }
 
 interface IItemProps {
     todo: ITodo;
+    onDelete: () => void;
 }
 
 interface IItemState {
@@ -17,11 +18,32 @@ export default class ToDo extends React.Component<IItemProps, IItemState> {
     constructor(props: IItemProps) {
         super(props);
         this.state = {done: false};
+        this.toggleDone = this.toggleDone.bind(this);
+    }
+    toggleDone() {
+        this.setState({done: !this.state.done});
     }
     render() {
-        return  <div style={
-            {width: '150px', margin: '5px', display: 'block', border: '1px solid black', borderRadius: '5px', padding: '5px', paddingLeft: '8px'}}>
+        const todoStyle = {
+            width: '250px',
+            margin: '5px',
+            display: 'block',
+            border: '1px solid black',
+            borderRadius: '5px',
+            padding: '5px',
+            paddingLeft: '8px',
+            backgroundColor: this.state.done ?'gray': 'white'
+        };
+
+        return  <div style={todoStyle}>
+            <Checkbox style={{float: 'left', marginRight: '2px'}} onClick={this.toggleDone}/>
+
             {this.props.todo.task}
+            <div style={{float: 'right'}}>
+                <button style={{border: 'none', cursor: 'pointer'}} onClick={this.props.onDelete}>
+                    <Icon name='window close'/>
+                </button>
             </div>
+        </div>
     }
 }
